ab9a8558de57719730caa823c7741744f08f4a42
Brahim Ziyani <brahimziyani@live.fr>
2024-04-02 07:16:00 +0200

    Navbar and Hero

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
+import React, { useState } from 'react';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Yo</h1>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" exact />
+        </Routes>
+      </Router>
+      <div className="centeredText">
+        <h1>Charles Cantin</h1>
+        <p>Photographe</p>
+      </div>
+    </>
   );
 }
+
 
 export default App;
