239934e3fc9d60f91003d1fe13aa23ada1802df6
Brahim Ziyani <brahimziyani@live.fr>
2024-04-14 18:51:15 +0200

    Background Home

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,12 +1,21 @@
 import React, { useState } from 'react';
 import './Hero.css'
 import Button from './Button';
+import { useEffect } from 'react';
 
 
 // Define a React functional component named Hero
 // This component represents a hero section with a title, subtitle, and buttons for Portfolio and Contact Me
 function Hero() {
-  const backgroundImage = './hero.jpg';
+  useEffect(() => {
+    // Add the specific CSS class to the body element when the component mounts
+    document.body.classList.add('hero-background');
+
+    // Remove the specific CSS class from the body element when the component unmounts
+    return () => {
+        document.body.classList.remove('hero-background');
+    };
+}, []); // Empty dependency array ensures this effect runs only once
 return (
   <div className="centeredText">
   
