c645c5d4d40d11beca48ee0e3fb981909aed3290
Brahim Ziyani <brahimziyani@live.fr>
2024-04-14 18:13:54 +0200

    Form Working

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,32 +1,39 @@
 import React from 'react';
+import { useForm } from '@formspree/react';
 import './Contact.css';
 
 
 // Define a React functional component named ContactPage
 // This component renders a contact form with input fields for name, email, subject, a send button, and a textarea for a message
 function ContactPage() {
+  const [state, handleSubmit] = useForm("mjvnpzpr");
+  if (state.succeeded) {
+      return <p>Thanks for joining!</p>;
+  }
+
+
   return (
     <div className='contact'>
       <h1>Contact Form</h1>
-      <div className='container'>
+      <form action='https://formspree.io/f/mjvnpzpr' className='container' onSubmit={handleSubmit} method='POST'>
         <div className='left-container'>
           <div className='input-container'>
-            <input type='text' placeholder='Name' />
+            <input name='name' type='text' placeholder='Name' />
           </div>
           <div className='input-container'>
-            <input type='email' placeholder='Email' />
+            <input name='email' type='email' placeholder='Email' />
           </div>
           <div className='input-container'>
-            <input type='text' placeholder='Subject' />
+            <input name='subject' type='text' placeholder='Subject' />
           </div>
           <div className='button-container'>
-            <button className='send-button'>Send</button>
+            <button type="submit" className='send-button'>Send</button>
           </div>
         </div>
         <div className='right-container'>
           <textarea placeholder='Message ...' />
         </div>
-      </div>
+      </form>
     </div>
   );
 }
